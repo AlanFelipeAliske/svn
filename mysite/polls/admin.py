208fa8c2d60beb6ef58d7b51c5b7964ec1865d9f
Alan Felipe Aliske <alanfelipealiske@gmail.com>
2022-01-12 13:51:20 -0300

    update

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from polls.models import Evento
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criação')
+    list_filter = ('usuario', 'titulo',)
+
+admin.site.register(Evento, EventoAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descrição = models.TextField(blank=True, null=True)
+    data_evento = models.DateField(verbose_name='Data do evento')
+    data_criação = models.DateField(auto_now=True, verbose_name='Data da criação')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
+        
