580d9a66619d9eb421569bb7ffbd98772cadda30
Alan <alanfelipealiske@gmail.com>
2022-01-25 18:38:49 -0300

    update

--- devMysite/polls/urls.py
+++ devMysite/polls/urls.py
@@ -6,10 +6,8 @@ from polls import views, admin
 from django.views.generic import RedirectView
 
 urlpatterns = [
+
     #path('admin/', admin.site.urls),
-    path('agenda/', views.lista_eventos),
     #path('', RedirectView.as_view(url='/polls/agenda/')),
-
     #path('', views.index),
-
 ]
